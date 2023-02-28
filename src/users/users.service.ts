58dc4387e7796a93ec486935b86a4e8a333b5b26
skylogin <skylogin@gmail.com>
2023-02-28 23:28:37 +0900

    service 추가

---
+++ src/users/users.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import * as uuid from 'uuid';
+
+import { EmailService } from 'src/email/email.service';
+import { UserInfo } from './UserInfo';
+
+@Injectable()
+export class UsersService {
+  constructor(private emailService: EmailService) {}
+
+  async createUser(name: string, email: string, password: string) {
+    await this.checkUserExists(email);
+
+    const signupVerifyToken = uuid.v1();
+
+    await this.saveUser(name, email, password, signupVerifyToken);
+    await this.sendMemberJoinEmail(email, signupVerifyToken);
+  }
+
+  verifyEmail(signupVerifyToken: string): Promise<string> {
+    throw new Error('Method not implemented.');
+  }
+
+  login(email: string, password: string): Promise<string> {
+    throw new Error('Method not implemented.');
+  }
+
+  async getUserInfo(userId: string): Promise<UserInfo> {
+    throw new Error('Method not implemented.');
+  }
+
+  private checkUserExists(email: string) {
+    return false;
+  }
+
+  private saveUser(
+    name: string,
+    email: string,
+    password: string,
+    signupVerifyToken: string,
+  ) {
+    return;
+  }
+
+  private async sendMemberJoinEmail(email: string, signupVerifyToken: string) {
+    await this.emailService.sendMemberJoinVerification(
+      email,
+      signupVerifyToken,
+    );
+  }
+}
